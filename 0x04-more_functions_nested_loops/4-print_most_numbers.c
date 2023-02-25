427b21584e67024d31f4338f54eb93dcfc98932a
mrlolman <ah1196@fayoum.edu.eg>
2023-02-24 17:06:10 -0800

    fggfg

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_most_numbers - print num except 2 and 4
  *
@@ -11,7 +13,7 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-_putchar(c);
+			_putchar(c);
 	}
 	_putchar('\n');
 }
